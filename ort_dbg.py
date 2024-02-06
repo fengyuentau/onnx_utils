bc1eddfc9d9fcef72840ca9dabc0ce2ea2a6e186
Yuantao Feng <yuantao.feng@opencv.org.cn>
2024-02-06 11:41:15 +0000

    add ort dbg mmode

--- ort_dbg.py
+++ ort_dbg.py
@@ -4,6 +4,7 @@ import onnxruntime as ort # build ort with RelWithDebugInfo to get more logs
 so = ort.SessionOptions()
 # Log severity level. Applies to session load, initialization, etc. 0:Verbose, 1:Info, 2:Warning. 3:Error, 4:Fatal. Default is 2.
 so.log_severity_level = 0
+so.log_verbosity_level = 1
 net = ort.InferenceSession("/path/to/model.onnx", providers=["CPUExecutionProvider"], sess_options=so)
 # some logs
 net.run()
